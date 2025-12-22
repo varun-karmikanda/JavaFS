a35884fcd8fb5da34fe05026133cca12f8aadc30
varun-karmikanda <varunbkarmikanda@gmail.com>
2025-12-22 16:12:44 +0530

    Add (Abstraction) Abstraction class to demonstrate abstraction principles in OOP

---
+++ Java/OOP/src/com/abstraction/Abstraction.java
@@ -0,0 +1,9 @@
+package com.abstraction;
+
+public class Abstraction {
+
+    // ABSTRACTION: Hide the internal complexity
+    // Basically hide the complex working and just give the desired result/working.
+    // Hiding Implementation Details
+
+}
