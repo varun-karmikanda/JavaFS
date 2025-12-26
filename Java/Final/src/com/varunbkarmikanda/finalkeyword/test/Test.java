c9091b914e109c4ce830e7bef9b4a8d7da214be1
varun-karmikanda <varunbkarmikanda@gmail.com>
2025-12-27 00:46:48 +0530

    Add (Final) implement Utils class with static methods for max and min calculations

---
+++ Java/Final/src/com/varunbkarmikanda/finalkeyword/test/Test.java
@@ -0,0 +1,8 @@
+package com.varunbkarmikanda.finalkeyword.test;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(Utils.PI);
+//        Utils.PI = 1.67; // Not possible as it is final
+    }
+}
---
+++ Java/Final/src/com/varunbkarmikanda/finalkeyword/test/Utils.java
@@ -0,0 +1,17 @@
+package com.varunbkarmikanda.finalkeyword.test;
+
+public class Utils {
+
+    public static final double PI = 3.142;
+
+    public static int max(int a, int b){
+        if(a > b) return a;
+        return b;
+    }
+
+    public static int min(int a, int b){
+        if(a < b) return a;
+        return b;
+    }
+
+}
