4f4a7cd4beadaf8e338cc4187fee6c87d8cdaea3
varun-karmikanda <varunbkarmikanda@gmail.com>
2025-12-29 23:31:45 +0530

    Add (Exceptions) implement Demo and Exceptions classes with basic structure and division logic

---
+++ Java/Exceptions/src/com/varunbkarmikanda/exception/Demo.java
@@ -0,0 +1,16 @@
+package com.varunbkarmikanda.exception;
+
+public class Demo {
+    public static void main(String[] args) {
+        int[] numerators = {10, 20, 30, 40};
+        int[] denominator = {10, 2, 0, 7};
+        for (int i = 0;i< numerators.length;i++){
+            System.out.println(divide(numerators[i], denominator[i]));
+        }
+        System.out.println("Good job!");
+    }
+
+    public static int divide(int a, int b){
+        return a / b;
+    }
+}
---
+++ Java/Exceptions/src/com/varunbkarmikanda/exception/Exceptions.java
@@ -0,0 +1,10 @@
+package com.varunbkarmikanda.exception;
+
+public class Exceptions {
+
+    // A particular case where a generalized case does not work
+    // Exception: Event that disrupts the normal flow of the program. It is a object that is thrown at runtime
+
+    // Exception Handling: a way to handle runtime errors so that normal flow of the application can be maintained
+
+}
