7b0ce2dc4dc77d3f39a5512d913a73bc898613a0
varun-karmikanda <varunbkarmikanda@gmail.com>
2025-12-30 14:11:32 +0530

    Add (Unchecked Exceptions) implement Student and UncheckedException classes for handling unchecked exceptions

---
+++ Java/Exceptions/src/com/varunbkarmikanda/exception/uncheckedexception/UncheckedException.java
@@ -0,0 +1,12 @@
+package com.varunbkarmikanda.exception.uncheckedexception;
+
+public class UncheckedException {
+
+    // Unchecked exception: type of exception which is not checked during the compile time but throws error at runtime
+
+    public static void main(String[] args) {
+        Student a = null;
+        a.setName("Bob");
+    }
+
+}
