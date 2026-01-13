37145dedcc08dbf28cdafb843a39e073b36a85a8
varun-karmikanda <varunbkarmikanda@gmail.com>
2026-01-13 22:23:08 +0530

    Add (Reentrant) ReentrantExample class demonstrating reentrant locking with nested method calls

---
+++ Java/Multithreading/src/com/varunbkarmikanda/multithreading/locks/reentrant/ReentrantExample.java
@@ -0,0 +1,33 @@
+package com.varunbkarmikanda.multithreading.locks.reentrant;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantExample {
+
+    private final Lock lock = new ReentrantLock();
+
+    public void outerMethod(){
+        lock.lock();
+        try{
+            System.out.println("Outer method");
+            innerMethod();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void innerMethod(){
+        lock.lock();
+        try{
+            System.out.println("Inner method");
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        ReentrantExample example = new ReentrantExample();
+        example.outerMethod();
+    }
+}
