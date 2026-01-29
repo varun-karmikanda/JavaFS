6f0c467ac10fb44892b7f5ac91ea3892d47db893
varun-karmikanda <varunbkarmikanda@gmail.com>
2026-01-29 22:25:24 +0530

    Add (Generics[Generic Interface]) Implement Container interface and its GenericContainer and StringContainer classes for type-safe item management

---
+++ Java/Generics/src/com/varunbkarmikanda/genericstypes/genericinterface/Container.java
@@ -0,0 +1,8 @@
+package com.varunbkarmikanda.genericstypes.genericinterface;
+
+public interface Container<T> {
+
+    void add(T item);
+
+    T get();
+}
---
+++ Java/Generics/src/com/varunbkarmikanda/genericstypes/genericinterface/StringContainer.java
@@ -0,0 +1,16 @@
+package com.varunbkarmikanda.genericstypes.genericinterface;
+
+public class StringContainer implements Container<String>{
+
+    private String item;
+
+    @Override
+    public void add(String item) {
+        this.item = item;
+    }
+
+    @Override
+    public String get() {
+        return item;
+    }
+}
