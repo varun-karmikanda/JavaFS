9528d1fe4ca54baf91a34880bf8b69dedb1752e6
varun-karmikanda <varunbkarmikanda@gmail.com>
2026-01-29 22:25:12 +0530

    Add (Generics[Generic Classes]) Implement Pair and Box classes with examples for type safety and value management

---
+++ Java/Generics/src/com/varunbkarmikanda/genericstypes/genericclass/pairgenerics/Main.java
@@ -0,0 +1,9 @@
+package com.varunbkarmikanda.genericstypes.genericclass.pairgenerics;
+
+public class Main {
+    public static void main(String[] args){
+        Pair<String, Integer> pair = new Pair<>("Roll No", 67);
+        System.out.println(pair.getKey());
+        System.out.println(pair.getValue());
+    }
+}
