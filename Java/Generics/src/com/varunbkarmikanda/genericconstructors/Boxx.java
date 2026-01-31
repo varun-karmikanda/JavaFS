f76770eaafae1e972a186059e979b66237e9ac2e
varun-karmikanda <varunbkarmikanda@gmail.com>
2026-01-31 22:52:36 +0530

    Add (Generics[Generic Constructors]) Implement Box and Boxx classes for type-safe number handling

---
+++ Java/Generics/src/com/varunbkarmikanda/genericconstructors/Boxx.java
@@ -0,0 +1,13 @@
+package com.varunbkarmikanda.genericconstructors;
+
+public class Boxx {
+    public <T extends Number> Boxx(T value){
+
+    }
+
+    public static void main(String[] args) {
+//        Boxx boxx = new Boxx("Varun");
+//        Boxx boxx = new Boxx(67);
+
+    }
+}
