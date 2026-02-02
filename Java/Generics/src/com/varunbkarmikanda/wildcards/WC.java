6a7b4ff5a5ca216c00e2a223a46af2ad20d342e5
varun-karmikanda <varunbkarmikanda@gmail.com>
2026-02-02 23:36:36 +0530

    Add (Generics[Wildcards]) Implement WC class with methods for handling wildcards in lists

---
+++ Java/Generics/src/com/varunbkarmikanda/wildcards/WC.java
@@ -0,0 +1,34 @@
+package com.varunbkarmikanda.wildcards;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class WC {
+
+    public static void main(String[] args){
+//        WC.sum(Arrays.asList(2,1));
+
+        List<? super Integer> list = Arrays.asList(1, 2, 3);
+        list.add(67);
+        list.add(null);
+
+    }
+
+    // Upper bound
+    public static double sum(List<? extends Number> numbers){
+        double sum = 0;
+        for (Number o: numbers){
+            sum += o.doubleValue();
+        }
+        return sum;
+    }
+
+    // Lower bound
+    public static void printNumbers(List<? super Integer> list){
+        for (Object o: list){
+            System.out.println(o);
+        }
+    }
+
+}
